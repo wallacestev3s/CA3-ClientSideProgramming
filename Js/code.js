6284cacae6b5222989e8044424ad603f0cb4bab3
Wallace Esteves <wallace_esteves@hotmail.com>
2021-05-10 00:12:39 +0100

    creating style to the policie password

--- Js/code.js
+++ Js/code.js
@@ -4,3 +4,11 @@ function _id(name){
 function _class(name){
     return document.getElementsByClassName(name);
 }
+_class("toggle-password")[0].addEventListener("click", function(){
+    _class("toggle-password")[0].classList.toggle("active");
+    if(_id("password-content").getAttribute("type")=="password"){
+        _id("password-content").setAttribute("type","text"); 
+    } else{
+        _id("password-content").setAttribute("type","password");
+    }
+});
