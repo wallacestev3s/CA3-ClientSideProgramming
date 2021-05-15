627333e47318faa71bcf0edd30f74ee95728ec68
Wallace Esteves <wallace_esteves@hotmail.com>
2021-05-15 23:17:59 +0100

    Recreating index login and js script

--- Js/code.js
+++ Js/code.js
@@ -1,52 +1,18 @@
-function _id(name){
-    return document.getElementById(name);
-}
-function _class(name){
-    return document.getElementsByClassName(name);
-}
-_class("toggle-password")[0].addEventListener("click", function(){
-    _class("toggle-password")[0].classList.toggle("active");
-    if(_id("password-content").getAttribute("type")=="password"){
-        _id("password-content").setAttribute("type","text"); 
-    } else{
-        _id("password-content").setAttribute("type","password");
-    }
-});
-
-
-_id("password-content").addEventListener("focus",function(){
-    _class("password-policies")[0].classList.add("active");
-});
-_id("password-content").addEventListener("blur",function(){
-    _class("password-policies")[0].classList.remove("active");
-});
-
-_id("password-content").addEventListener("keyup",function(){
-    let password = _id("password-content").value;
-
-    if(/[A-Z]/.test(password)){
-        _class("policy-uppercase")[0].classList.add("active");
-    } else {
-        _class("policy-uppercase")[0].classList.remove("active");
-    }
-
-    if(/[0-9]/.test(password)){  // maybe need to change that 
-        _class("policy-number")[0].classList.add("active");
-    } else {
-        _class("policy-number")[0].classList.remove("active");
-    }
-
-    if(/[^A-Za-z0-9]/.test(password)){
-        _class("policy-special")[0].classList.add("active");
-    } else {
-        _class("policy-special")[0].classList.remove("active");
-    }
-
-    if(password.length > 8){
-        _class("policy-length")[0].classList.add("active");
-    } else {
-        _class("policy-length")[0].classList.remove("active");
-    }
-
-
-});
+$(document).ready(function(){
+	
+	$('input[type=password]').keyup(function() {
+		var pswd = $(this).val();
+		
+		//validate the length
+		if ( pswd.length < 8 ) {
+			$('#length').removeClass('valid').addClass('invalid');
+		} else {
+			$('#length').removeClass('invalid').addClass('valid');
+		}
+		
+		//validate letter
+		if ( pswd.match(/[A-z]/) ) {
+			$('#letter').removeClass('invalid').addClass('valid');
+		} else {
+			$('#letter').removeClass('valid').addClass('invalid');
+		}
